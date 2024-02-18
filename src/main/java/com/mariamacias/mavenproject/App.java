431d69d3ced67b0166217fd0a11cb9ac5324a69e
Maria Macias <mmaciasp@my.centennialcollege.ca>
2024-02-18 18:44:22 -0500

    display greeting according to time of day. See #1

--- src/main/java/com/mariamacias/mavenproject/App.java
+++ src/main/java/com/mariamacias/mavenproject/App.java
@@ -1,5 +1,6 @@
 package com.mariamacias.mavenproject;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
 /**
  * Hello world!
  *
@@ -8,6 +9,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	// Get the current time
+        Date currentTime = new Date();
+        
+        // Create a SimpleDateFormat to extract the hour from the current time
+        SimpleDateFormat hourFormat = new SimpleDateFormat("HH");
+        int hour = Integer.parseInt(hourFormat.format(currentTime));
+
+        // Determine the time of day and print a greeting
+        String timeGreeting = "";
+        if (hour >= 5 && hour < 12) {
+            timeGreeting = "Good morning";
+        } else if (hour >= 12 && hour < 18) {
+        	timeGreeting = "Good afternoon";
+        } else {
+        	timeGreeting = "Good evening";
+        }
+        System.out.println(timeGreeting + ", Maria Macias, Welcome to COMP367");
     }
 }
